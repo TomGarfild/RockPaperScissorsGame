c0ad504a5c8ba0a3b95ca5534a981071dac37a89
Oleksii Safroniuk <osafroniuk@gmail.com>
2021-02-22 07:33:31 +0200

    Add RegistrationController and Account model

---
+++ RockPaperScissorsGame/Server/Controllers/RegistrationController.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server
+{
+    [ApiController]
+    [Route("api/v1/registration")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class RegistrationController : ControllerBase
+    {
+        
+    }
+}
---
+++ RockPaperScissorsGame/Server/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace Server
+{
+    public class Account
+    {
+        public string Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
