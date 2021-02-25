402a0b556c523205d67aa4b202e392eb6329e447
IvanHalych <39216494+IvanHalych@users.noreply.github.com>
2021-02-25 13:33:27 +0200

    Change get token instead login

--- RockPaperScissorsGame/Server/Controllers/StatisticController.cs
+++ RockPaperScissorsGame/Server/Controllers/StatisticController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Server.Service;
+using Server.Services;
 using Server.StatisticStorege;
 
 namespace Server.Controllers
@@ -13,21 +14,34 @@ namespace Server.Controllers
     [Route("api/v1/statistic")]
     public class StatisticController : ControllerBase
     {
+        private readonly IAuthService _authService;
+
+        public StatisticController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
         [HttpGet]
         [Route("LocalStatistic")]
-        public  Task<IEnumerable<StatisticItem>> Local([FromHeader(Name = "x-login")][Required] string user,
+        public async Task<ActionResult<List<StatisticItem>>> Local([FromHeader(Name = "x-token")][Required] string token,
         [FromServices] StatisticService statisticService)
         {
-            // todo check authorization
-            return Task.FromResult(statisticService.GetStatisticItems(user));
+            if (!_authService.IsAuthorized(token))
+            {
+                var user = _authService.GetLogin(token);
+                return statisticService.GetStatisticItems(user).ToList();
+            }
+            else
+            {
+                return StatusCode(401);
+            }
         }
         [HttpGet]
         [Route("GlobalStatistic")]
-        public Task<IEnumerable<StatisticItem>> Global([FromHeader(Name = "x-login")][Required] string user,
+        public async Task<List<StatisticItem>> Global([FromHeader(Name = "x-login")][Required] string user,
             [FromServices] StatisticService statisticService)
         {
-            // todo check authorization
-            return Task.FromResult(statisticService.GetGlobalStatistic());
+            return statisticService.GetGlobalStatistic().ToList();
         }
 
     }
