9d38c794ae76d96804f3252f14f35982459c649d
Oleksii Safroniuk <osafroniuk@gmail.com>
2021-02-25 15:17:42 +0200

    Building version

--- RockPaperScissorsGame/Client/GameMenu.cs
+++ RockPaperScissorsGame/Client/GameMenu.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text.Json;
 using System.Threading.Tasks;
-using Client;
-using Server;
-using Server.Model;
 
-namespace _httpClient
+namespace Client
 {
     public class GameMenu : Menu
     {
--- RockPaperScissorsGame/Client/RegistrationMenu.cs
+++ RockPaperScissorsGame/Client/RegistrationMenu.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using _httpClient;
 using Server.Models;
 
 namespace Client
